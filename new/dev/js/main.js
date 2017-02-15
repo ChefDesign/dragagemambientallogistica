b951a393a7c6bd1eddd85ea49599d41d6ab2ac0c
Denis Toledo <contato@denistoledo.com.br>
2017-02-15 21:35:50 -0200

    Finalizando validacao

--- new/dev/js/main.js
+++ new/dev/js/main.js
@@ -28,7 +28,8 @@ var Ambiental = {
     Ambiental.fixedNav();
     Ambiental.videoAnchor();
     Ambiental.selectForm();
-    Ambiental.jqueryValidation();
+    Ambiental.jqueryValidationMobile();
+    Ambiental.jqueryValidationDesktop();
   },
   /**
    * fixedNav
@@ -103,15 +104,15 @@ var Ambiental = {
   /**
    * jqueryValidation
    * @access public
-   * @desc form validation
+   * @desc mobile form validation
    *
    * @return {Void}
    */
-  jqueryValidation: function() {
+  jqueryValidationMobile: function() {
     'use strict';
 
-    var btn = $('#enviar'),
-        form = $('#form');
+    var botaoEnviarMobile = $('#btnMobile'),
+        formularioMobile = $('#form-mobile');
 
     jQuery.validator.setDefaults({
         errorClass: 'error',
@@ -119,69 +120,149 @@ var Ambiental = {
         validClass: 'valid'
     });
 
-    btn.on('click', function() {
-      form.validate({
-        rules: {
-          name: {
-            required: true,
-            minlength: 3
-          },
-          email: {
-            required: true,
-            email: true
-          }
+    formularioMobile.validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3
         },
-        messages: {
-          name: {
-            required: 'Digite seu nome',
-            minlength: jQuery.validator.format('Mínimo {0} caracteres')
-          },
-          email: {
-            required: 'Digite seu e-mail',
-            email: 'Insira um e-mail válido'
-          }
-        },
-        highlight: function(element, errorClass, validClass) {
-          $(element).addClass(errorClass).removeClass(validClass);
-        },
-        unhighlight: function(element, errorClass, validClass) {
-          $(element).removeClass(errorClass).addClass(validClass);
-        },
-        submitHandler: function(form) {
-          var dados = $(form).serialize();
-
-          $.ajax({
-            type: 'POST',
-            url: 'processa.php',
-            data: dados,
-            dataType: 'text',
-            cache: false,
-            beforeSend: function() {
-              btn.val('Enviando...');
-            },
-            complete: function() {
-              btn.val('Processando...');
-            },
-            success: function() {
-              setTimeout(function() {
-                btn.addClass('sucesso').val('Obrigado pelo contato');
-
-                setTimeout(function() {
-                  btn.removeClass('sucesso').val('Solicitar proposta');
-                }, 2000);
-              }, 2000);
-
-              $('#name, #email, #phone').val('');
-            },
-            error: function(xhr, ajaxOptions, thrownError) {
-              console.log(xhr.status);
-              console.log(thrownError);
-            }
-          });
-
-          return false;
+        email: {
+          required: true,
+          email: true
         }
-      });
+      },
+      messages: {
+        name: {
+          required: 'Digite seu nome',
+          minlength: jQuery.validator.format('Mínimo {0} caracteres')
+        },
+        email: {
+          required: 'Digite seu e-mail',
+          email: 'Insira um e-mail válido'
+        }
+      },
+      highlight: function(element, errorClass, validClass) {
+        $(element).addClass(errorClass).removeClass(validClass);
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).removeClass(errorClass).addClass(validClass);
+      },
+      submitHandler: function(form) {
+        var dados = $(form).serialize();
+
+        $.ajax({
+          type: 'POST',
+          url: 'processa.php',
+          data: dados,
+          dataType: 'text',
+          cache: false,
+          beforeSend: function() {
+            botaoEnviarMobile.text('Enviando...');
+          },
+          complete: function() {
+            botaoEnviarMobile.text('Processando...');
+          },
+          success: function() {
+            setTimeout(function() {
+              botaoEnviarMobile.addClass('sucesso').text('Obrigado pelo contato');
+
+              setTimeout(function() {
+                botaoEnviarMobile.removeClass('sucesso').text('Solicitar proposta');
+              }, 2000);
+            }, 2000);
+
+            $('#name, #email, #phone').val('');
+          },
+          error: function(xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+          }
+        });
+
+        return false;
+      }
+    });
+  },
+  /**
+   * jqueryValidation
+   * @access public
+   * @desc desktop form validation
+   *
+   * @return {Void}
+   */
+  jqueryValidationDeskop: function() {
+    'use strict';
+
+    var botaoEnviarDesktop = $('#btnDesktop'),
+        formularioDesktop = $('#form-desktop');
+
+    jQuery.validator.setDefaults({
+        errorClass: 'error',
+        errorElement: 'p',
+        validClass: 'valid'
+    });
+
+    formularioDesktop.validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3
+        },
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: 'Digite seu nome',
+          minlength: jQuery.validator.format('Mínimo {0} caracteres')
+        },
+        email: {
+          required: 'Digite seu e-mail',
+          email: 'Insira um e-mail válido'
+        }
+      },
+      highlight: function(element, errorClass, validClass) {
+        $(element).addClass(errorClass).removeClass(validClass);
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).removeClass(errorClass).addClass(validClass);
+      },
+      submitHandler: function(form) {
+        var dados = $(form).serialize();
+
+        $.ajax({
+          type: 'POST',
+          url: 'processa.php',
+          data: dados,
+          dataType: 'text',
+          cache: false,
+          beforeSend: function() {
+            botaoEnviarDesktop.text('Enviando...');
+          },
+          complete: function() {
+            botaoEnviarDesktop.text('Processando...');
+          },
+          success: function() {
+            setTimeout(function() {
+              botaoEnviarDesktop.addClass('sucesso').text('Obrigado pelo contato');
+
+              setTimeout(function() {
+                botaoEnviarDesktop.removeClass('sucesso').text('Solicitar proposta');
+              }, 2000);
+            }, 2000);
+
+            $('#name, #email, #phone').val('');
+          },
+          error: function(xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+          }
+        });
+
+        return false;
+      }
     });
   }
 }
